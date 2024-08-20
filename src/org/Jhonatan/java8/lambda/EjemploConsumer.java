494c0d03f2ba5b41cb69b45ec15b2d0f4ff52b8f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-19 22:09:50 -0500

    class EjemploConsumer: mas ejemplos de expresiones lambdas ahora con Objetos

--- src/org/Jhonatan/java8/lambda/EjemploConsumer.java
+++ src/org/Jhonatan/java8/lambda/EjemploConsumer.java
@@ -1,12 +1,21 @@
 package org.Jhonatan.java8.lambda;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import org.Jhonatan.java8.lambda.Models.Usuario;
 
 public class EjemploConsumer {
     public static void main(String[] args) throws Exception {
+        ejemploSupplierUsuario();
+    }
+
+    public static void ejemplo01() {
         System.out.println("\n\t=EXPRESIONES LAMBDA: EJEMPLO CONSUNER=");
 
         System.out.println("=EJEMPLO SALUDO=");
@@ -30,9 +39,70 @@ public class EjemploConsumer {
 
         /* cuando son dos o mas parametros se usan parametros */
         BiConsumer<String, Integer> consumidorBi = (nombre, edad) -> {
-            System.out.println(nombre+" tiene "+edad+" años.");
+            System.out.println(nombre + " tiene " + edad + " años.");
         };
 
         consumidorBi.accept("PEPE", 56);
+    }
+
+    public static void ejemplo02() {
+        System.out.println("\n\t=OTRO EJEMPLO CON CONSUMER SIMPLEFICADO=");
+
+        /* se utilixan en apis de java como el Strem */
+        Consumer<String> consumidor02 = System.out::println;
+        consumidor02.accept("HOLA MUNDO!");
+
+        System.out.println("\n\t=EJEMPLO CON LISTAS=");
+        List<String> listaNombres = Arrays.asList("andres", "pepe", "luz", "paco");
+        listaNombres.forEach(consumidor02);
+
+        System.out.println("\n\t=EJEMPLO DE USUARIO=");
+        Usuario usuario = new Usuario();
+        BiConsumer<Usuario, String> asignarNombre = (persona, nombre) -> {
+            persona.setNombre(nombre);
+        };
+
+        asignarNombre.accept(usuario, "Andrez");
+        System.out.println("Nombre de usuario: " + usuario.getNombre());
+    }
+
+    public static void ejemplUsuarioSimpleficado() {
+
+        System.out.println("\n\t=EJEMPLO DE USUARIO SIMPLEFICADO=");
+
+        Usuario usuario = new Usuario();
+        BiConsumer<Usuario, String> asignarNombre = Usuario::setNombre;
+        asignarNombre.accept(usuario, "Andrez");
+        System.out.println("Nombre de usuario: " + usuario.getNombre());
+
+    }
+
+    public static void ejemploSupplier() {
+       
+        /*la cual no resive ningun argumento y devuelve un valor de aacuerdo al tipo de dato */
+        System.out.println("\n\t=EJEMPLO Supplier=");
+        Supplier<String> provedor = () -> {
+            return "Hola mundo lambda supplier.";
+        };
+
+        /*forma simpleficada */
+        Supplier<String> provedor02 = () -> "Hola Mundo Lambda supplier simpleficado.";
+
+        System.out.println(provedor.get());
+        System.out.println(provedor02.get());
+    }
+
+    public static void ejemploSupplierUsuario(){
+
+        System.out.println("\n\t=EJEMPLO Supplier USUARIO=");
+        Supplier<Usuario> crearUsuario = Usuario::new;
+        Usuario usuario = crearUsuario.get();
+
+        BiConsumer<Usuario,String> asignarNombre = Usuario::setNombre;
+        asignarNombre.accept(usuario, "Andrez");
+
+        System.out.println("Nombre usuario: "+usuario.getNombre());
+
+
     }
 }
---
+++ src/org/Jhonatan/java8/lambda/Models/Usuario.java
@@ -0,0 +1,15 @@
+package org.Jhonatan.java8.lambda.Models;
+
+public class Usuario {
+
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
